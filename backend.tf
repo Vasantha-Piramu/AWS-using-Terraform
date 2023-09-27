c68b34d20115e99cfb6d1e429cc9399fa3fcbe14
Vasantha <vasantha1221@outlook.com>
2023-09-27 13:30:03 +0100

    removed backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-/*terraform {
-  backend "s3" {
-    bucket         = "tf_s3_BK"
-    key            = "test/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "terraform-lock"
-  }
-}*/
